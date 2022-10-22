bf0082cebda67a3229e8559625b969e4cbbc3b32
amite <amit.erandole@gmail.com>
2022-10-22 16:52:26 +0530

    primary routes setup

---
+++ app/routes/index.tsx
@@ -0,0 +1,10 @@
+export default function IndexRoute() {
+  return (
+    <div>
+      <h2> Hello Index Route</h2>
+      <p>
+        <a href="/jokes">Want some jokes?</a>
+      </p>
+    </div>
+  );
+}
---
+++ app/routes/jokes/index.tsx
@@ -0,0 +1,12 @@
+export default function JokesIndexRoute() {
+  return (
+    <div>
+      <p>Here's a random joke:</p>
+      <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+
+      <p>
+        <a href="/jokes/new">Tell me a new one</a>
+      </p>
+    </div>
+  );
+}
